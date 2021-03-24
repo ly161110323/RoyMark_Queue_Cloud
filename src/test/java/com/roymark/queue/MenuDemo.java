1a921035ed439ef1f42325be8259ddf1ab7a4c10
ly161110323 <871842433@qq.com>
2021-03-24 15:42:46 +0800

    3.24-3

--- src/test/java/com/roymark/queue/MenuDemo.java
+++ src/test/java/com/roymark/queue/MenuDemo.java
@@ -1,11 +1,9 @@
 package com.roymark.queue;
 
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.roymark.queue.dao.QueueMenuinfoMapper;
-import com.roymark.queue.entity.QueueMenuinfo;
-import com.roymark.queue.service.QueueMenuinfoService;
+import com.roymark.queue.entity.Window;
+import com.roymark.queue.service.WindowService;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,68 +25,16 @@ public class MenuDemo {
 
 
     @Autowired
-    private QueueMenuinfoMapper queueMenuinfoMapper;
-    @Autowired
-    private QueueMenuinfoService queueMenuinfoService;
+    private WindowService windowService;
+    
     @Test
     public void demo1(){
-        List <QueueMenuinfo> menuList=queueMenuinfoService.searchMenuInfoListByAccountLsAndAreaLs("7665f65747a44b8a87ded0a729e713d2",9,true);
-        for(QueueMenuinfo menu:menuList)
+        List <Window> windows =windowService.list();
+        for(Window window : windows)
         {
-            System.out.println("menuName:"+menu.getMenuName());
+            System.out.println("window name: "+window.getName());
         }
     System.out.println("****************************************");
     }
-//    @Test
-//    public void demo2(){
-//        QueryWrapper<HospitalqueueMenuinfo> menuinfoQueryWrapper=new QueryWrapper<HospitalqueueMenuinfo>();
-//        menuinfoQueryWrapper.eq("Menu_IsShow","1");
-//        menuinfoQueryWrapper.eq("status","1");
-//        List<HospitalqueueMenuinfo>     menuList=hospitalqueueMenuinfoMapper.selectList(menuinfoQueryWrapper);
-//        for(HospitalqueueMenuinfo menu:menuList)
-//        {
-//            System.out.println("menuName:"+menu.getMenuName());
-//        }
-//        System.out.println("****************************************");
-//    }
 
-//    @Test
-//    public void demo3(){
-//        QueryWrapper<HospitalqueueMenuinfo> menuinfoQueryWrapper=new QueryWrapper<HospitalqueueMenuinfo>();
-//        menuinfoQueryWrapper.eq("Menu_IsShow","1");
-//        menuinfoQueryWrapper.eq("status","1");
-//        List<HospitalqueueMenuinfo>     menuList= hospitalqueueMenuinfoService.searchMenuInfoList(menuinfoQueryWrapper,true);
-//        for(HospitalqueueMenuinfo menu:menuList)
-//        {
-//            System.out.println("menuName:"+menu.getMenuName()+"是否有子菜单:"+menu.getIsHaveChildren());
-//        }
-//        System.out.println("****************************************");
-//    }
-
-//    @Test
-//    public void demo4(){
-//
-//        List<HospitalqueueMenuinfo>     menuList= hospitalqueueMenuinfoService.searchMenuInfoListByAccountLs("13",true);
-//        for(HospitalqueueMenuinfo menu:menuList)
-//        {
-//            System.out.println("menuName:"+menu.getMenuName()+"是否有子菜单:"+menu.getIsHaveChildren());
-//        }
-//        System.out.println("****************************************");
-//    }
-
-//    @Test
-//    public void demo5(){
-//        Page<HospitalqueueMenuinfo> page = new Page<HospitalqueueMenuinfo>(1,2);
-//        HospitalqueueMenuinfo menu=new HospitalqueueMenuinfo();
-//        menu.setMenuLevel("1");
-//
-//
-//    IPage<HospitalqueueMenuinfo> pageList=hospitalqueueMenuinfoService.searchPageMenuInfoList(page,menu);
-//    List<HospitalqueueMenuinfo> menuList=pageList.getRecords();
-//        for(HospitalqueueMenuinfo tempMenu:menuList)
-//        {
-//            System.out.println("menuName:"+tempMenu.getMenuName());
-//        }
-//        System.out.println("****************************************");
-//    }
 }
